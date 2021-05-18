17acd54fac55817ed5f70f69e847ef5bb14fea6b
alaaahmeed <alaa.aahmeedd@gmail.com>
2021-05-18 16:07:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,57 @@
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+
+
+var a = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (a == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 2000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    a = 1;
+  }
+
+});
+
+$(document).ready(function(){
+  $(".hov").hover(function(){
+    $(this).css("box-shadow", "3px 3px 3px 3px #dedede");
+    }, function(){
+      $(this).css("box-shadow", "0px 0px 0px 0px #dedede");
+  });
+});
